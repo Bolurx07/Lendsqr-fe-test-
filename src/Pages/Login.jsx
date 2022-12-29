e21cf3dd13ce47bd5c5012be8ae8b478eb40db92
Israel <iaboluwatife@gmail.com>
2022-12-29 16:26:14 -0500

    Installed scss and organized folder/directory

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,29 @@
+import '../Stylesheets/Login.scss'
+import logo from '../Assets/mainlogo.svg'
+import loginimg from '../Assets/loginbackgroundimg.svg'
+const Login = () => {
+  return (
+    <div className='loginContainer'>
+
+        <section className='loginsect1'>
+            <img src={logo} alt="logo" />
+            <div>
+                <img className='bckgrndimg' src={loginimg} alt="loginimg" />
+            </div>
+        </section>
+        
+        <section className='loginsect2'>
+            <h2>Welcome!</h2>
+            <p>Enter details to login</p>
+            <form action="">
+                <input type="email" name="email" placeholder='Email' id='email' />
+                <input type="password" name="password" placeholder='Password' id='password' />
+                <p>Forgot Password</p>
+                <button type="submit">Login</button>
+            </form>
+        </section>
+    </div>
+  )
+}
+
+export default Login
